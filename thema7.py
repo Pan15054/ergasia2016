78326161154fb68d2960900795c026bc7611197a
Pan15054 <panba7381@gmail.com>
2016-03-09 15:18:02 +0200

    Added files via upload

---
+++ thema7.py
@@ -0,0 +1,23 @@
+﻿import urllib2
+import json
+print "give geographic coordinates"
+print "(becareful with the coordinates you give)"
+x=raw_input("lat: ")
+y=raw_input("lon: ")
+url='http://api.openweathermap.org/data/2.5/weather?lat='+x+'&lon='+y+'&appid=d89ab85c7700a2ee6e26475397209c20'
+json_obj=urllib2.urlopen(url)#για να ανοιξουμε τη σελιδα με τα δεδομενα
+data=json.load(json_obj)#για να αποθηκευσουμε τα δεδομενα που παιρνουμε
+for i in data['weather']:
+    if i['main']=='Rain':
+        print 'i am singing in the rain'
+K=data['main']['temp']#μετατροπη της θερμοκρασιας Kelvin που μας δινεται σε celsius
+celsius=K-273.15
+print celsius
+if celsius>20:
+    print 'nice'
+elif celsius<5:
+    print 'brrr...'
+
+
+        
+
---
+++ thema8.py
@@ -0,0 +1,28 @@
+﻿f=open('pinakas.txt','r')
+list1=f.readlines()
+space=' '
+pinakas=[[space for x in range(0,8)] for x in range(0,8)]
+y='0'
+k=0
+for i in range(0,8):
+    for j in range(0,41):
+        if list1[i][j]==y:
+            pinakas[i].pop(k)           #αλγοριθμος ο οποιος θα αποθηκευση τον πινακα που περνει 
+            pinakas[i].insert(k,y)      #απο το αρχειο
+            k+=1
+        elif j==40:
+            k=0
+        elif list1[i][j]==space and list1[i][j-1]=="'":
+            k+=1
+for i in range(0,8):
+    print pinakas[i]
+def rotations(pinakas,degree,W):
+    j=degree/90
+    W[j]=zip(*pinakas[::-1]) #η συναρτηση η οποια θα κανει τον πινακα να περισταφει
+    while j!=0:              #αναλογα με το συνολο των μοιρων που θελουμε
+        for i in W[j]:
+            print i
+        j=j-1
+        W[j]=zip(*W[j+1][::-1])
+W={"1":1,"2":2,"3":3}
+rotations(pinakas,270,W)
